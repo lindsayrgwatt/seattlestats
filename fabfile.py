8cd08cd84e96a0b81c6e7ec42f15c7d714d8116c
lindsayrgwatt <lindsayrgwatt@gmail.com>
2015-07-26 21:00:05 -0700

    Updated deployment scripts to set up environment

--- fabfile.py
+++ fabfile.py
@@ -10,71 +10,69 @@ env.github_url = "https://github.com/lindsayrgwatt/seattlestats"
 env.http_dir = '/home/ubuntu'
 env.project_name = 'seattlestats'
 env.project_dir = os.path.join(env.http_dir, env.project_name)
-env.hosts = ['ec2-54-69-157-24.us-west-2.compute.amazonaws.com']
+env.virtualenv_dir = os.path.join(env.project_dir, 'venv')
+env.hosts = ['ec2-52-10-132-110.us-west-2.compute.amazonaws.com']
 env.user = 'ubuntu'
 env.sudo_user = env.user
-env.postgres_user_password = '1234abcd'
+env.postgres_user_password = 'iluvpostgres'
 env.postgres_project_database = 'seattlestats'
 env.postgres_project_user = 'seattlestats'
 env.postgres_project_user_password = '1234abcd'
 
-def hello(name="world"):
-    print("Hello %s!" % name)
-
-def fake_deploy(name="test"):
-    local("echo test")
-    print(green("This text is green!"))
-
-def fake_remote_deploy():
-    run("echo fake deploy")
-
 def update_server():
-    run("sudo apt-get update")
-    run("sudo apt-get upgrade")
-    run("sudo apt-get dist-upgrade")
+    run("sudo apt-get -y -q update")
+    run("sudo apt-get -y -q upgrade")
+    # All the options in the following command are to avoid getting stuck on a grub update screen
+    # http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
+    run('sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade')
 
 def install_required_software():
-    run("sudo apt-get install -y -q python-dev") # Needed to avoid GCC compilation error for pycrypto
-    run("sudo apt-get install -y -q nginx")
-    run("sudo apt-get install -y -q supervisor")
-    run("sudo service supervisor restart")
-    run("sudo apt-get install -y -q git")
-    run("sudo apt-get install -y -q binutils libproj-dev gdal-bin") # Geospatial libraries
-    run("sudo apt-get install -y -q postgresql postgresql-contrib python-psycopg2")
-    run("sudo apt-get install -y -q postgis postgresql-9.3-postgis-2.1")
-    run("sudo apt-get install -y -q build-essential libxml2-dev libgdal-dev libproj-dev libjson0-dev xsltproc docbook-xsl docbook-mathml") # PostGIS
+    run("sudo apt-get -y -q install python-dev") # Needed to avoid GCC compilation error for pycrypto
+    run("sudo apt-get -y -q install nginx")
+    run("sudo apt-get -y -q install git")
+    run("sudo apt-get -y -q install binutils libproj-dev gdal-bin") # Geospatial libraries
+    run("sudo apt-get -y -q install postgresql postgresql-contrib python-psycopg2")
+    run("sudo apt-get -y -q install postgis postgresql-9.3-postgis-2.1")
+    run("sudo apt-get -y -q install build-essential libxml2-dev libgdal-dev libproj-dev libjson0-dev xsltproc docbook-xsl docbook-mathml") # PostGIS
 
 def start_nginx():
     sudo("service nginx start")
 
 def install_python_packages():
-    run("sudo apt-get install -y -q python-pip")
+    run("sudo apt-get -y -q install python-pip")
     run("sudo pip install --upgrade pip")
     run("sudo pip install virtualenv")
-    run("sudo pip install virtualenvwrapper")
-    run("virtualenv %(project_name)s" % env) # Creates virtualenv in project dir
-    run("source %(project_name)s/bin/activate" % env)
-    run("pip install gunicorn")
 
 def install_redis():
-    run("sudo apt-get install -y -q redis-server") # Will restart on reboot
+    run("sudo apt-get -y -q install redis-server") # Will restart on reboot
 
 def test_redis():
     run("sudo redis-cli ping | grep 'PONG'")
 
 def configure_postgres():
-    run("sudo psql -U postgres -c \"ALTER USER postgres WITH PASSWORD '%(postgres_user_password)s';\"" % env)
+    print "Now you need to do two things:"
+    print "1. Give the postgres user in your database a password"
+    print "2. Update the authentication method of postgres so that you can actually connect"
+    print "Some details here: http://suite.opengeo.org/4.1/dataadmin/pgGettingStarted/firstconnect.html"
+    print "Part 1."
+    print "1. ssh into your server: ssh %(user)s@%(hosts)s" % env
+    print "2. open psql as the postgres user. No password is required as none exists yet: sudo -u postgres psql postgres"
+    print "3. change the password: \password postgres"
+    print "4. quit: \q"
+    print "5. end session: exit"
+    print "\n"
+    print "Part 2."
     run("sudo find / -name \"pg_hba.conf\" -print")
     print "Now you need to do the following to change peer authentication to md5"
-    print "SSH into your server e.g., ssh env.user@env.hosts"
-    print "Change the file listed in print statement above e.g., sudo vi /etc/postgresql/9.3/main/pg_hba.conf"
-    print "Change the lines:"
+    print "1. SSH into your server as above"
+    print "2. Change the file listed in print statement above e.g., sudo vi /etc/postgresql/9.3/main/pg_hba.conf"
+    print "3. Change the lines:"
     print "local   all             postgres                                peer"
     print "local   all             all                                     peer"
     print "to:"
     print "local   all             postgres                                md5"
     print "local   all             all                                     md5"
-    print "Now manually run fab restart_postgres"
+    print "4. Save, end the SSH session and from your local machine run: fab restart_postgres"
 
 def restart_postgres():
     run("sudo service postgresql restart")
@@ -88,25 +86,6 @@ def configure_server():
     test_redis()
     configure_postgres()
 
-def deploy():
-    # Install or update the code
-    with settings(warn_only=True):
-        if run('test -d ' + env.project_dir).failed:
-            run("sudo git clone %(github_url)s" % env) # First install of code
-        else:
-            with cd(env.project_dir):
-                run("sudo git pull")
-    
-    # Install or update any required packages
-    with prefix('source %(project_dir)s/bin/activate' % env):
-        with cd(env.project_dir):
-            run("pip install -r requirements.txt") # DO NOT use sudo here or will install outside your virtualenv
-
-    # http://nurupoga.org/articles/deployment-with-fabric-and-virtualenv
-    # http://stackoverflow.com/questions/9337149/is-virtualenv-recommended-for-django-production-server -> scroll down
-    # http://dangoldin.com/2014/02/10/using-virtualenv-in-production/
-    # http://thecodeship.com/deployment/deploy-django-apache-virtualenv-and-mod_wsgi/
-
 def set_up_database():
     run("sudo psql -U postgres -c \"CREATE DATABASE %(postgres_project_database)s;\"" % env)
     run("sudo psql -U postgres -c \"CREATE USER %(postgres_project_user)s WITH PASSWORD '%(postgres_project_user_password)s';\"" % env)
@@ -116,7 +95,46 @@ def set_up_database():
 
 def test_database_created():
     run("sudo psql -U %(postgres_project_user)s <<EOF\n\c %(postgres_project_database)s\nSELECT postgis_full_version();\nEOF" % env)
-    # Should see something like
+    # Should see something like:
+    #                                                                          postgis_full_version                                                                         
+    # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    #  POSTGIS="2.1.2 r12389" GEOS="3.4.2-CAPI-1.8.2 r3921" PROJ="Rel. 4.8.0, 6 March 2012" GDAL="GDAL 1.10.1, released 2013/08/26" LIBXML="2.9.1" LIBJSON="UNKNOWN" RASTER
+    # (1 row)
+
+def deploy():
+    # Install or update the code
+    with settings(warn_only=True):
+        if run('test -d ' + env.project_dir).failed:
+            # First install of code
+            run("sudo git clone %(github_url)s" % env) # First install of code
+
+            # Set up virtualenv and change write permissions
+            run("sudo virtualenv %(virtualenv_dir)s" % env)
+            run("sudo chmod -R 777 %(virtualenv_dir)s" % env)
+
+        else:
+            with cd(env.project_dir):
+                run("sudo git pull")
+    
+    # Install or update any required packages
+    with prefix('source %(virtualenv_dir)s/bin/activate' % env):
+        with cd(env.project_dir):
+            run("pip install -r requirements.txt") # DO NOT use sudo here or will install outside your virtualenv
+            # You can see this packages installed at env.virtualenv_dir/lib/python2.7/site-packages
+
+def install_and_configure_gunicorn():
+    with prefix('source %(virtualenv_dir)s/bin/activate' % env):
+        run("pip install gunicorn")
+
+
+#run("sudo apt-get install -y -q supervisor")
+#run("sudo service supervisor restart")
+    
+# http://nurupoga.org/articles/deployment-with-fabric-and-virtualenv
+# http://stackoverflow.com/questions/9337149/is-virtualenv-recommended-for-django-production-server -> scroll down
+# http://dangoldin.com/2014/02/10/using-virtualenv-in-production/
+# http://thecodeship.com/deployment/deploy-django-apache-virtualenv-and-mod_wsgi/
+
 
 #def first_deploy():
     
@@ -129,3 +147,14 @@ def test_database_created():
     # Create environment variables
 
     # Add celery
+
+def hello(name="world"):
+    print("Hello %s!" % name)
+
+def fake_deploy(name="test"):
+    local("echo test")
+    print(green("This text is green!"))
+
+def fake_remote_deploy():
+    run("echo fake deploy")
+
